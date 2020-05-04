3d3e06ba07354974ee23fa13cd49dc02cf7a1366
培哥 <andyli0123boy@gmail.com>
2020-05-04 09:48:19 +0800

    新增註解

--- 20200504_播放旋律實習-小蜜蜂.ino
+++ 20200504_播放旋律實習-小蜜蜂.ino
@@ -1,3 +1,10 @@
+/*
+ * 播放旋律實習-小蜜蜂
+ * 麵包板A：GND、蜂鳴器一腳(8Ω/0.5W)
+ * 麵包板B：蜂鳴器一腳(8Ω/0.5W)、電容器短腳(1000uF/25V)
+ * 麵包板C：PWM 2、電容器長腳(1000uF/25V)
+*/
+
 const int speaker = 2;
 char toneName[] = "CDEFGAB";
 unsigned int frequency[7] = {523, 587, 659, 694, 784, 880, 988};
